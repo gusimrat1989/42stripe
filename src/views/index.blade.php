b686f17c2ab5dd0a2c14a0c4b7146685106a8161
Gursimrat Singh <gursimrat@42works.net>
2019-02-20 16:03:51 +0530

    Changes

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,64 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>42 Stripe</title>
+<div class="row">
 
-	<!---::::: Bootstrap CSS :::::-->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+	@foreach( $data as $get )
 
-	<!--::::: Custom Css :::::-->
-	<style>
+		<div class="col-md-3">
 
-	.stripe42-image img
-	{
-		width: 200px;
-		height: 160px;
-	}
+			<div class="stripe42-image">
+				<img style="width: 100px; height:100px" src="{{ $get['image'] }}">
+			</div>
 
-	</style>
+			<div class="stripe42-price">
+				<span>{{ $get['amount'] }} {{ $get['currency'] }}</span>
+			</div>
 
-	<!---::::: Bootstrap JS :::::-->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+			<div class="stripe42-buy">
 
-</head>
-<body>
-	<div class="container">
-		<div class="row">
+				<form action="{{ URL::to( 'stripe42/charge' ) }}" method="post">
 
-			@foreach( $data as $get )
+					{!! csrf_field() !!}
 
-				<div class="col-md-3">
+					<input type="hidden" name="amount" value="{{ $get['amount'] }}">
+					<input type="hidden" name="currency" value="{{ $get['currency'] }}">
+					<input type="hidden" name="customer_id" value="{{ $get['customer_id'] }}">
+					<input type="submit" class="btn btn-primary" value="Buy">
 
-					<div class="stripe42-image">
-						<img src="{{ $get['image'] }}">
-					</div>
+				</form>
 
-					<div class="stripe42-price">
-						<span>{{ $get['amount'] }} {{ $get['currency'] }}</span>
-					</div>
-
-					<div class="stripe42-buy">
-
-						<form action="{{ URL::to( 'stripe42/charge' ) }}" method="post">
-
-							{!! csrf_field() !!}
-
-							<input type="hidden" name="amount" value="{{ $get['amount'] }}">
-							<input type="hidden" name="currency" value="{{ $get['currency'] }}">
-							<input type="hidden" name="customer_id" value="{{ $get['customer_id'] }}">
-							<input type="submit" class="btn btn-primary" value="Buy">
-
-						</form>
-
-					</div>
-
-				</div>
-
-			@endforeach
+			</div>
 
 		</div>
-	</div>
-</body>
-</html>
+
+	@endforeach
+
+</div>
